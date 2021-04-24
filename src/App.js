d58c465e1078c41ad298ba9dddc393a3e3488656
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-24 16:46:13 +0530

    Done with Sidebar. Building Chat component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+import { Counter } from './features/Counter';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
     <div className="app">
-
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
