fa9fbfb54e2690dd6ea0f50d9803fe8ada9b0b30
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-24 16:53:32 +0530

    creating chat header

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import ChatHeader from '../ChatHeader/ChatHeader'
 import "./Chat.css"
 
 const Chat = () => {
     return (
         <div className="chat">
-
+            <ChatHeader />
         </div>
     )
 }
---
+++ src/components/ChatHeader/ChatHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ChatHeader.css"
+
+const ChatHeader = () => {
+    return (
+        <div className="chatHeader">
+
+        </div>
+    )
+}
+
+export default ChatHeader
