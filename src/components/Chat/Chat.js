afb2de8865040834703ebc58a0b6d5b8a8b6ca61
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-24 20:59:21 +0530

    created chat header and input box.

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,3 +1,4 @@
+import { AddCircle, CardGiftcard, EmojiEmotions, Gif } from '@material-ui/icons'
 import React from 'react'
 import ChatHeader from '../ChatHeader/ChatHeader'
 import "./Chat.css"
@@ -6,6 +7,21 @@ const Chat = () => {
     return (
         <div className="chat">
             <ChatHeader />
+            <div className="chat_messages">
+
+            </div>
+            <div className="chat_input">
+                <AddCircle fontSize="large" />
+                <form>
+                    <input type="text" placeholder={`Message #bleh`} />
+                    <button className="chat_inputButton" type="submit">Send Message</button>
+                </form>
+                <div className="chat_inputIcons">
+                    <CardGiftcard fontSize="large" />
+                    <Gif fontSize="large" />
+                    <EmojiEmotions fontSize="large" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/ChatHeader/ChatHeader.js
+++ src/components/ChatHeader/ChatHeader.js
@@ -1,10 +1,30 @@
+import { EditLocationRounded, HelpRounded, Notifications, PeopleAltRounded, SearchRounded, SendRounded } from '@material-ui/icons'
 import React from 'react'
 import "./ChatHeader.css"
 
 const ChatHeader = () => {
     return (
         <div className="chatHeader">
+            <div className="chatHeader_left">
+                <h3>
+                    <span className="chatHeader_hash">
+                        #
+                    </span>
+                    Test Channel Name
+                </h3>
+            </div>
+            <div className="chatHeader_right">
+                <Notifications />
+                <EditLocationRounded />
+                <PeopleAltRounded />
 
+                <div className="chatHeader_search">
+                    <input type="text" placeholder="Search" />
+                    <SearchRounded />
+                </div>
+                <SendRounded />
+                <HelpRounded />
+            </div>
         </div>
     )
 }
