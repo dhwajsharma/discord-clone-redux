a55006e2d4a0dd170323e20acaf0b73b6788567b
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-25 16:26:39 +0530

    Implemented SignIn and SignOut functionality.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from './features/userSlice';
 import Login from './components/Login/Login';
+import { auth } from './firebase';
+import { login, logout } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user is logged in
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName,
+        }))
+
+      } else {
+        // the user is logged out
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
 
 
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,15 @@
 import { Button } from '@material-ui/core'
+import { auth, provider } from "../../firebase"
 import React from 'react'
 import "./Login.css"
 
 const Login = () => {
     const signIn = () => {
+        auth.signInWithPopup(provider)
+            .catch((error) => alert(error.message));
 
     }
+
     return (
         <div className="login">
             <div className="login_logo">
