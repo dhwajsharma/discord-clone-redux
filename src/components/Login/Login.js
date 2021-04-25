85738f4dcc4c4ebcffac6c077bb5c32a4f5ba812
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-25 14:32:48 +0530

    Done with login page. Going to implement google auth

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import "./Login.css"
+
+const Login = () => {
+    const signIn = () => {
+
+    }
+    return (
+        <div className="login">
+            <div className="login_logo">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pC2G9IJ1xOOzLdYoP3pnZgHaHZ%26pid%3DApi&f=1" alt="" />
+            </div>
+            <Button variant="contained" color="primary" onClick={signIn}>Sign in</Button>
+        </div>
+    )
+}
+
+export default Login
